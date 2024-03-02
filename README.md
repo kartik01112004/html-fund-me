f365fbbd9fc0f97312afbce675855a4c8d4245c5
kartik01112004 <kartikgoel018@gmail.com>
2024-03-02 14:33:18 +0530

    all added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 1. HTML/JavaScript
    later on Nextjs/React
+
+This is a frontend of our fund me project
+we always need 2 seperate folders one for backend with smartcontracts and one with frontend
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import { ethers } from "./ethers-5.6.esm.min.js";
 import { abi, contractAddress } from "./constants.js";
+
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fund");
+const balanceButton = document.getElementById("balanceButton");
+const withdrawButton = document.getElementById("withdrawButton");
 connectButton.onclick = connect;
+balanceButton.onclick = getBalance;
 fundButton.onclick = fund;
+withdrawButton.onclick = withdraw;
 
 async function connect() {
   if (typeof window.ethereum != "undefined") {
@@ -18,8 +23,16 @@ async function connect() {
   }
 }
 
+async function getBalance() {
+  if (typeof window.ethereum != "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const balance = await provider.getBalance(contractAddress);
+    console.log(ethers.utils.formatEther(balance));
+    console.log(`${balance.toString()} wei`);
+  }
+}
 async function fund() {
-  const ethAmount = "0.1";
+  const ethAmount = document.getElementById("ethAmount").value;
   console.log(`funding with ${ethAmount}... `);
   if (typeof window.ethereum != "undefined") {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -48,4 +61,19 @@ function listenForTranscationMine(transactionResponce, provider) {
       resolve();
     });
   });
+}
+
+async function withdraw() {
+  if (typeof window.ethereum != "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    console.log(signer);
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponce = await contract.withdraw();
+      await listenForTranscationMine(transactionResponce, provider); //listen for tx to be  mined
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
